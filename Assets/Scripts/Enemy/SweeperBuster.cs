e28fbef57defba6a76dee3fc1ffe2a7b4d4ad89f
martin-newbie <g.martin.min@gmail.com>
2024-04-27 13:26:47 +0900

    [#update] 콜라이더 조절, 자폭병 멀어지면 삭제되게

--- Assets/Scripts/Enemy/SweeperBuster.cs
+++ Assets/Scripts/Enemy/SweeperBuster.cs
@@ -12,21 +12,26 @@ public class SweeperBuster : Enemy
     {
         base.InitEnemy(_target, retire);
         moveDir = (target.position - transform.position).normalized;
-        retireAction += RetireExplosion;
     }
 
     protected override void MoveLogic()
     {
         transform.Translate(moveDir * Time.deltaTime * moveSpeed);
         sprite.flipX = moveDir.x > 0;
+
+        if (Vector3.Distance(target.position, transform.position) > 25f)
+        {
+            retireAction?.Invoke(this);
+        }
     }
 
     protected override void TriggerStayWithPlayer(Collider2D collision)
     {
         retireAction?.Invoke(this);
+        RetireExplosion();
     }
 
-    void RetireExplosion(Entity subject)
+    void RetireExplosion()
     {
         isAlive = false;
         var expl = Instantiate(explosive, transform.position, Quaternion.identity);
