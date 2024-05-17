89510c93db702a6fae9b6c343dd0163b8700a86e
Black_Penguin <dkstmdwnswns@gmail.com>
2024-05-18 02:52:54 +0900

    [#update] 타이틀 화면 제작

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void GameQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
