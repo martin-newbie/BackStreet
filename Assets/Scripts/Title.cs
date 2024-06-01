1d19c77e85215c6329103cbfc6b9275d1baef2a7
martin-newbie <g.martin.min@gmail.com>
2024-06-01 20:50:08 +0900

    타이틀 수정 및 버튼 포커스 움직임 추가

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -5,6 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    int focusIndex;
+    public RectTransform buttonFocus;
+    public RectTransform[] buttonsRect;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            focusIndex--;
+            if(focusIndex < 0)
+            {
+                focusIndex += buttonsRect.Length;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            focusIndex++;
+        }
+
+        int clampedFocusIdx = Mathf.Abs(focusIndex % buttonsRect.Length);
+        var targetButton = buttonsRect[clampedFocusIdx];
+        buttonFocus.anchoredPosition = Vector2.Lerp(buttonFocus.anchoredPosition, targetButton.anchoredPosition, Time.deltaTime * 25f);
+    }
+
     public void GameStart()
     {
         SceneManager.LoadScene("Main");
